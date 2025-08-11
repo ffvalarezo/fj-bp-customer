2c8cc1b7a5d0f68e650451de90f38155e74f9be7
Francisco Valarezo <frvalare@pichincha.com>
2025-08-11 17:05:22 -0500

    feat: data

--- account-service/bin/main/data.sql
+++
@@ -1,7 +0,0 @@
--- Cuentas asociadas a clientes existentes
-INSERT INTO account (account_number, account_type, initial_balance, status, customer_id) VALUES
-('478758', 'SAVINGS', 1425.00, true, 1),
-('225487', 'CURRENT', 700.00, true, 2),
-('495878', 'SAVINGS', 150.00, true, 3),
-('496825', 'SAVINGS', 0.00, true, 2),
-('585545', 'CURRENT', 1000.00, true, 1);
